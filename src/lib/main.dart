ac1b5a626afcd565ae8739cc2aab0127975048ff
k-kasama <kasakei0633@yahoo.co.jp>
2021-08-21 19:56:56 +0900

    flutter_hooks,hooks_riverpod導入

--- src/lib/main.dart
+++ src/lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(ProviderScope(child:MyApp()));
 }
 
 class MyApp extends StatelessWidget {
